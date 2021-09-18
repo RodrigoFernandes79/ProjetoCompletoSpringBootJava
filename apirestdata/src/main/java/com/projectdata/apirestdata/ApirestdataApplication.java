3b428e2b38ee05ce569794b2785f6aa236b4c5fb
RodrigoFernandes79 <rodrigohf79@hotmail.com>
2021-09-17 21:46:05 -0300

    Project ApiRestData Created

---
+++ apirestdata/src/main/java/com/projectdata/apirestdata/ApirestdataApplication.java
@@ -0,0 +1,13 @@
+package com.projectdata.apirestdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestdataApplication.class, args);
+	}
+
+}
---
+++ apirestdata/src/test/java/com/projectdata/apirestdata/ApirestdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdata.apirestdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
